0e96aa00b72ae8431a657785357327d0a4e22196
Pei Li <flyiei.jason@gmail.com>
2023-02-26 18:04:11 +0200

    main: add first api

---
+++ src/main/java/com/solid/microservices/limitsservice/bean/Limits.java
@@ -0,0 +1,31 @@
+package com.solid.microservices.limitsservice.bean;
+
+public class Limits {
+
+    private int minimun;
+    private int maximun;
+
+    public Limits() {
+    }
+
+    public Limits(int minimun, int maximun) {
+        this.minimun = minimun;
+        this.maximun = maximun;
+    }
+
+    public int getMinimun() {
+        return minimun;
+    }
+
+    public void setMinimun(int minimun) {
+        this.minimun = minimun;
+    }
+
+    public int getMaximun() {
+        return maximun;
+    }
+
+    public void setMaximun(int maximun) {
+        this.maximun = maximun;
+    }
+}
